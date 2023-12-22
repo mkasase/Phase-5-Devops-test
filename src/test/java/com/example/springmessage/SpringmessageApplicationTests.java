75e3a1ac34de7f6c02f8440078f7d374cd92fec7
mwila kasase <mkasase@gmail.com>
2023-12-22 09:42:12 +0000

    init commit

---
+++ src/test/java/com/example/springmessage/SpringmessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
